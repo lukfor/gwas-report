632fb4685bd398263911ea1923cce130123021fc
Lukas Forer <lukas.forer@i-med.ac.at>
2023-03-08 14:08:15 +0100

    Prepare release 0.3.0

--- src/main/java/com/github/lukfor/App.java
+++ src/main/java/com/github/lukfor/App.java
@@ -12,7 +12,7 @@ public class App {
 
 	public static final String APP = "gwas-report";
 
-	public static final String VERSION = "0.2.0";
+	public static final String VERSION = "0.3.0";
 
 	public static final String URL = "https://github.com/lukfor/gwas-report";
 
